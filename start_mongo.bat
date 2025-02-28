da14bd7e16999f3e96a20d1bf36fb78c2bfde13b
RomanQed <gbakaldin@gmail.com>
2025-02-28 10:41:37 +0300

    Improve stat_mongo.bat

--- start_mongo.bat
+++ start_mongo.bat
@@ -2,11 +2,26 @@
 setlocal
 
 if not exist "mongo.id" (
-    docker run -i -d -p 27017:27017 mongo:latest >> mongo.id
+    docker pull mongo:latest
+    docker run -i -d -p 27017:27017 --pull=never mongo:latest >> mongo.id
+    if errorlevel 1 (
+        del mongo.id >nul 2>&1
+        exit /b 1
+    )
     exit /b 0
 )
 
 set /p id=<mongo.id
+
+if "%id%"=="" (
+    docker pull mongo:latest
+    docker run -i -d -p 27017:27017 --pull=never mongo:latest >> mongo.id
+    if errorlevel 1 (
+        del mongo.id >nul 2>&1
+        exit /b 1
+    )
+    exit /b 0
+)
 
 docker start %id% >nul 2>&1
 
